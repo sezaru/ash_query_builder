f3e554fb9a9b67cd4969aef8528d7384382eda4b
Eduardo <sezdocs@dev-distrobox.fedora.local>
2023-07-23 16:28:24 +0000

    fix(sorter): Always prepend a sorter

--- lib/ash_query_builder/to_query.ex
+++ lib/ash_query_builder/to_query.ex
@@ -7,7 +7,7 @@ defmodule AshQueryBuilder.ToQuery do
     query = filters |> Enum.filter(& &1.enabled?) |> Enum.reduce(query, &Protocol.to_filter/2)
 
     Enum.reduce(sorters, query, fn sorter, query ->
-      Ash.Query.sort(query, [{sorter.field, sorter.order}])
+      Ash.Query.sort(query, [{sorter.field, sorter.order}], prepend?: true)
     end)
   end
 end
