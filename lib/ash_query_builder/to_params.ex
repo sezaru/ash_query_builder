60a758094318a5bc802fa380bc31b1804e6591bc
Eduardo <sezdocs@dev-distrobox.fedora.local>
2023-08-27 20:45:34 +0000

    improvement: Add FilterScope support for ToParams

--- lib/ash_query_builder/to_params.ex
+++ lib/ash_query_builder/to_params.ex
@@ -1,7 +1,7 @@
 defmodule AshQueryBuilder.ToParams do
   @moduledoc false
 
-  alias AshQueryBuilder.Filter.Protocol
+  alias AshQueryBuilder.{FilterScope, Filter.Protocol}
 
   def generate(builder, opts) do
     with_disabled? = Keyword.get(opts, :with_disabled?, false)
@@ -9,7 +9,7 @@ defmodule AshQueryBuilder.ToParams do
     filters = filters_to_params(builder.filters, with_disabled?)
     sorters = sorters_to_params(builder.sorters)
 
-    params = %{"f" => filters, "s" => sorters}
+    params = %{f: filters, s: sorters}
 
     params |> :erlang.term_to_binary([{:compressed, 1}]) |> Base.encode64()
   end
@@ -17,20 +17,33 @@ defmodule AshQueryBuilder.ToParams do
   defp filters_to_params(filters, with_disabled?) do
     filters
     |> maybe_filter_out_disabled(with_disabled?)
-    |> Enum.map(fn filter ->
-      id = filter.id
+    |> Enum.map(fn
+      %FilterScope{} = scope ->
+        id = scope.id
 
-      values = %{
-        "p" => filter.path,
-        "f" => filter.field,
-        "o" => Protocol.operator(filter),
-        "v" => filter.value
-      }
+        values = %{
+          o: scope.operation,
+          f: filters_to_params(scope.filters, with_disabled?),
+          t: :scope
+        }
 
-      values =
-        values |> maybe_add_enabled_flag(filter, with_disabled?) |> maybe_add_metadata(filter)
+        {id, values}
 
-      {id, values}
+      filter ->
+        id = filter.id
+
+        values = %{
+          p: filter.path,
+          f: filter.field,
+          o: Protocol.operator(filter),
+          v: filter.value,
+          t: :filter
+        }
+
+        values =
+          values |> maybe_add_enabled_flag(filter, with_disabled?) |> maybe_add_metadata(filter)
+
+        {id, values}
     end)
     |> Enum.into(%{})
   end
@@ -40,7 +53,7 @@ defmodule AshQueryBuilder.ToParams do
     |> Enum.map(fn sorter ->
       id = sorter.id
 
-      {id, %{"f" => sorter.field, "o" => sorter.order}}
+      {id, %{f: sorter.field, o: sorter.order}}
     end)
     |> Enum.into(%{})
   end
@@ -49,8 +62,8 @@ defmodule AshQueryBuilder.ToParams do
   defp maybe_filter_out_disabled(filters, true), do: filters
 
   defp maybe_add_enabled_flag(values, _, false), do: values
-  defp maybe_add_enabled_flag(values, filter, true), do: Map.put(values, "e", filter.enabled?)
+  defp maybe_add_enabled_flag(values, filter, true), do: Map.put(values, :e, filter.enabled?)
 
   defp maybe_add_metadata(values, %{metadata: nil}), do: values
-  defp maybe_add_metadata(values, %{metadata: metadata}), do: Map.put(values, "m", metadata)
+  defp maybe_add_metadata(values, %{metadata: metadata}), do: Map.put(values, :m, metadata)
 end
