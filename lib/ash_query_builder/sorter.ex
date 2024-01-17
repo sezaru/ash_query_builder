2031dbebfc1a08779e8d688151097cd96a9c7b2b
Jeferson Vieira Ramos <jeferson.ramos@rebuilt.com>
2024-01-17 14:47:29 -0300

    feat: Adds *_nils_last support

--- lib/ash_query_builder/sorter.ex
+++ lib/ash_query_builder/sorter.ex
@@ -1,12 +1,16 @@
 defmodule AshQueryBuilder.Sorter do
   @moduledoc false
 
-  @type t :: %__MODULE__{id: non_neg_integer, field: atom, order: :asc | :desc}
+  @type t :: %__MODULE__{
+          id: non_neg_integer,
+          field: atom,
+          order: :asc | :asc_nils_last | :desc | :desc_nils_last
+        }
 
   defstruct [:id, :field, :order]
 
   def new(id \\ :erlang.unique_integer([:monotonic, :positive]), field, order) do
-    true = order in [:desc, :asc]
+    true = order in [:desc, :desc_nils_last, :asc, :asc_nils_last]
 
     struct!(__MODULE__, id: id, field: field, order: order)
   end
