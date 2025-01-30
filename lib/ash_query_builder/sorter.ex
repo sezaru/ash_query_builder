51946b391538b57529cd1bb35228498bd6124693
Eduardo B. Alexandre <sezdocs@github.com>
2025-01-30 18:26:10 -0300

    feat: Add support for args in sorters

--- lib/ash_query_builder/sorter.ex
+++ lib/ash_query_builder/sorter.ex
@@ -1,17 +1,25 @@
 defmodule AshQueryBuilder.Sorter do
   @moduledoc false
 
+  @type order :: :asc | :asc_nils_last | :desc | :desc_nils_last
+
   @type t :: %__MODULE__{
           id: non_neg_integer,
           field: atom,
-          order: :asc | :asc_nils_last | :desc | :desc_nils_last
+          order: order | {map, order}
         }
 
   defstruct [:id, :field, :order]
 
   def new(id \\ :erlang.unique_integer([:monotonic, :positive]), field, order) do
-    true = order in [:desc, :desc_nils_last, :asc, :asc_nils_last]
+    validate_order!(order)
 
     struct!(__MODULE__, id: id, field: field, order: order)
   end
+
+  defp validate_order!(order) when is_atom(order) do
+    true = order in [:desc, :desc_nils_last, :asc, :asc_nils_last]
+  end
+
+  defp validate_order!({_, order}), do: validate_order!(order)
 end
